7aee4ed39c2a9b8e24a88f83576e21008e596e1a
aryanair01 <aryanair1031@gmail.com>
2024-08-17 02:15:43 +0530

    Delete nodejs/database/db.js

--- nodejs/database/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const db = mongoose.connect("mongodb://127.0.0.1:27017/blogging").then(()=>{
-    console.log("database connected successfully")
-}).catch((error)=>{
-    console.log("database is not connected")
-})
